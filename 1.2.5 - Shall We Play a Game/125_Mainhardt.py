593d9b68dc0aceeed862eb6a293508e7835dffe4
BrockCollison <237946@shakopeeschools.org>
2025-10-27 09:15:00 -0500

    Creating milestones
    Added major milestones in comments.

---
+++ 1.2.5 - Shall We Play a Game/125_Mainhardt.py
@@ -0,0 +1,9 @@
+import turtle as trtl
+
+#TODO Create a leaderboard that displays a score based on how long the user has lasted.
+
+#TODO  Incorporate randomly placed  cars that will try to hit the player.
+
+#TODO  Make a collide feature where the game ends if the player comes in contact with a car.
+
+#TODO  Create the actual game layout where there are three columns in which the player can move 
